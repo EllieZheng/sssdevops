84284640aa091bdb45f3bc9219ad71c5d69ada12
Lianjun Zheng <lianjunzheng.pku09@gmail.com>
2018-07-10 09:55:50 -0400

    updates the mean funcion

--- sssdevops/sssdevops.py
+++ sssdevops/sssdevops.py
@@ -20,12 +20,8 @@ def mean(num_lst):
 
     Examples
     --------------
-    >>> mean([1,2,3,4,5])
+    >>> mean([1, 2, 3, 4, 5])
     3.0
     """
 
-    total = 0
-    for i in num_lst:
-        total += i
-    num = len(num_lst)
-    return total / num
+    return sum(num_lst) / len(num_lst)

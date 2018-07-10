43c28c4ab50c865a8f7440e6ce5764e9d302feaa
Lianjun Zheng <lianjunzheng.pku09@gmail.com>
2018-07-10 11:55:23 -0400

    adds codecov

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
             'tests': [
                 'pytest>=3.0',
                 'pytest-cov',
+                'codecov'
             ],
             'develop': [ #extra
                 'yapf',

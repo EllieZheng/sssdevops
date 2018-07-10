9fd9b2cadada9df5a9cea98aad7e089eb8f5e06b
Lianjun Zheng <lianjunzheng.pku09@gmail.com>
2018-07-10 10:12:22 -0400

    adds fixtures in test_mean.py

--- sssdevops/tests/test_mean.py
+++ sssdevops/tests/test_mean.py
@@ -4,6 +4,7 @@ This file is to test the mean function
 
 
 from sssdevops import mean
+import pytest 
 
 
 def test_simple():
@@ -11,3 +12,12 @@ def test_simple():
     observed = mean(num_list)
     expected = 1.5
     assert observed == expected
+
+
+@pytest.fixture # tell pytest that return a new, clean resource
+def num_list():
+    return [1, 2, 3, 4, 5]
+
+
+def test_more(num_list): # pytest will scan for all the fixtures and search for num_lists
+    assert mean(num_list) == 3.0

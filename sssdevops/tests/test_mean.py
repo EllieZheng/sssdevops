626034a70012008e2e7658801ca438e1dc8ff7ad
Lianjun Zheng <lianjunzheng.pku09@gmail.com>
2018-07-10 10:34:24 -0400

    adds more functions in test_mean.py

--- sssdevops/tests/test_mean.py
+++ sssdevops/tests/test_mean.py
@@ -7,6 +7,7 @@ from sssdevops import mean
 import pytest 
 
 
+@pytest.mark.skip
 def test_simple():
     num_list = [1, 2]
     observed = mean(num_list)
@@ -21,3 +22,22 @@ def num_list():
 
 def test_more(num_list): # pytest will scan for all the fixtures and search for num_lists
     assert mean(num_list) == 3.0
+
+
+def test_empty():
+    with pytest.raises(ZeroDivisionError):
+        mean([])
+
+
+def test_type_error():
+    with pytest.raises(TypeError):
+        mean(5)
+
+
+@pytest.mark.parametrize("num_list, expected_mean", [
+    (   [1, 2, 3, 4, 5],     3.0    ),
+    (   [2/3, 1/3],          0.5    ), 
+    (   [1.2],               1.2    ),
+])
+def test_many(num_list, expected_mean):
+    assert mean(num_list) == expected_mean

6e8ccd514c5853a6f2a6329bfda94266369f7d15
Lianjun Zheng <lianjunzheng.pku09@gmail.com>
2018-07-10 09:29:00 -0400

    inplement split

--- sssdevops/listtools.py
+++ sssdevops/listtools.py
@@ -2,5 +2,25 @@
 Miscellanenous list manipulation
 """
 
-def split(num_lst, index):
-    pass
+def split(in_list, index):
+    """
+    
+    Parameters
+    ----------
+    in_list: list
+    index: int
+
+    Returns
+    -------
+    Two lists, spliting in_list by index
+
+    Examples
+    --------
+    >>> split(['a', 'b', 'c', 'd'], 3) 
+    (['a', 'b', 'c'], []'d'])
+    """
+
+    list1 = in_list[:index]
+    list2 = in_list[index:]
+
+    return list1, list2
